0508418ae5ba3c0cdd2fda24629fda5667136f41
Chris Missal <chris.missal@gmail.com>
2013-01-08 13:30:14 -0600

    Update src/Freddie/Properties/AssemblyInfo.cs

--- src/Freddie/Properties/AssemblyInfo.cs
+++ src/Freddie/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Freddie")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Freddie (the MailChimp mascot) is a .NET API wrapper for MailChimp that keeps things simple.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Freddie")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Chris Missal 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Freddie.Tests")]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.*")]
