6df12257b9efd81a162153a565a29c43f9a4a8c3
Chris Missal <chris.missal@gmail.com>
2013-01-09 02:21:32 -0600

    pull mailchimp variables from ignored config

--- src/Tests/BaseTester.cs
+++ src/Tests/BaseTester.cs
@@ -1,5 +1,18 @@
+using System.Configuration;
+using NUnit.Framework;
+
 namespace Freddie.Tests
 {
+    [SetUpFixture]
+    public class MailchimpSetUpFixture
+    {
+        public MailchimpSetUpFixture()
+        {
+            BaseTester.Apikey = ConfigurationManager.AppSettings["ApiKey"];
+            BaseTester.MasterListId = ConfigurationManager.AppSettings["MasterListId"];
+        }
+    }
+
     public abstract class BaseTester
     {
         public static string Apikey = "";
